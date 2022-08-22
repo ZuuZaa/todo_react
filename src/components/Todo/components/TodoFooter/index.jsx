fd1033fb2830df5c0bdca81bd8e8efcb950f658e
ZuuZaa <zumbaghirova@gmail.com>
2022-08-22 10:34:53 +0400

    footer links css

--- src/components/Todo/components/TodoFooter/index.jsx
+++ src/components/Todo/components/TodoFooter/index.jsx
@@ -3,8 +3,23 @@ import "./style.scss";
 const TodoFooter = () => {
     return(
         <div className="todo-footer">
-            <h1>todo footer</h1>
-            
+            <div className="mobile-footer">
+                <div className="top-container">
+                    <div className="items-left">
+                        <span className="left">0</span>
+                        <span>items left</span>
+                    </div>
+                    <div className="clear-completed">
+                        clear completed
+                    </div>
+                </div>
+                <div className="bottom-container">
+                    <div className="all footer-link active">all</div>
+                    <div className="active-task footer-link">active</div>
+                    <div className="completed footer-link">completed</div>
+                </div>
+            </div>
+            <div className="desktop-footer"></div>
         </div>
     )
 };
