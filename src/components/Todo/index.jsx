924fed91bde142e774e4e98240ea6fd67373a299
ZuuZaa <zumbaghirova@gmail.com>
2022-09-16 23:04:24 +0400

    deploy

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -3,12 +3,10 @@ import TodoInput from "./components/TodoInput";
 import TodoFooter from "./components/TodoFooter";
 import { Outlet } from "react-router-dom";
 import { useTodo } from "contexts/todo";
-import _ from "lodash";
 import "./style.scss";
 
 
 const Todo = () => {
-  const { todoList } = useTodo();
 
   return (
     <div className="todo">
